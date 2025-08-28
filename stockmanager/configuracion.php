c1071e1c9d2fad7a1b8d6127761cece30ac48e71
Ramirez Navarro José Enrique <alu.2511183@itesrenedescartes.edu.mx>
2025-08-28 15:17:33 -0600

    configuracion.php

--- stockmanager/configuracion.php
+++ stockmanager/configuracion.php
@@ -1,6 +1,6 @@
 <?php //los parametros para entrar a la base de datos
-$server = "localhost";
-$user = "root";
-$pass = "Ramirezz394";
-$bd = "inventario";
+$server = "mysql-stockmanager.alwaysdata.net";
+$user = "428227";
+$pass = "Carlos2583&";
+$bd = "stockmanager_inventario";
 ?>
