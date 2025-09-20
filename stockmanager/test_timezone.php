7002610539c9e3872a1dadd8928fb3a3860bfec4
Ramirez Navarro José Enrique <alu.2511183@itesrenedescartes.edu.mx>
2025-09-20 15:26:58 -0600

    confirmo

--- stockmanager/test_timezone.php
+++
@@ -1,6 +0,0 @@
-<?php
-date_default_timezone_set('America/Mexico_City');
-echo "Zona horaria configurada: " . date_default_timezone_get() . "<br>";
-echo "Fecha y hora actual: " . date('Y-m-d H:i:s T') . "<br>";
-echo "Fecha para base de datos: " . date('Y-m-d H:i:s') . "<br>";
-?>
